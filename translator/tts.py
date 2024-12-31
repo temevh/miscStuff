09f76336d2cf08f38aa988c10c551ed661fba743
temevh <temehama@gmail.com>
2024-12-31 12:20:50 +0200

    removed comment

--- translator/tts.py
+++ translator/tts.py
@@ -13,7 +13,7 @@ with open('fluent625.txt', 'r', encoding='utf-8') as file:
 for word in words:
     word = word.strip().split(";")
     if len(word) < 2:
-        continue  # Skip lines that don't have both English and Korean words
+        continue
     english_word = word[0].strip()
     korean_word = word[1].strip()
     print(korean_word)
