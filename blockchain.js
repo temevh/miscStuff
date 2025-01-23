770b8b6d0f3a8a3993244f4b5ea54ef6e26fccf4
temevh <temehama@gmail.com>
2025-01-23 19:04:00 +0200

    basic blockchain functionality

--- blockchain.js
+++ blockchain.js
@@ -2,7 +2,7 @@ const SHA256 = require("crypto-js/sha256");
 
 class cryptoBlock {
   constructor(index, timestamp, data, precedingHash = " ") {
-    this.indes = index;
+    this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.precedingHash = precedingHash;
@@ -17,3 +17,38 @@ class cryptoBlock {
     ).toString();
   }
 }
+
+class CryptoBlockchain {
+  constructor() {
+    this.blockchain = [this.startGenesisBlock()];
+  }
+  startGenesisBlock() {
+    return new cryptoBlock(0, "23/01/2025", "Initial Block in the Chain", "0");
+  }
+  obtainLatestBlock() {
+    return this.blockchain[this.blockchain.length - 1];
+  }
+  addNewBlock(newBlock) {
+    newBlock.precedingHash = this.obtainLatestBlock().hash;
+    newBlock.hash = newBlock.computeHash();
+    this.blockchain.push(newBlock);
+  }
+}
+
+let testCoin = new CryptoBlockchain();
+testCoin.addNewBlock(
+  new cryptoBlock(1, "24/01/2025", {
+    sender: "John Smith",
+    recipient: "Will Smith",
+    quantity: 69,
+  })
+);
+testCoin.addNewBlock(
+  new cryptoBlock(1, "25/01/2025", {
+    sender: "Aladin",
+    recipient: "Will Smith",
+    quantity: 420,
+  })
+);
+
+console.log(JSON.stringify(testCoin, null, 4));
